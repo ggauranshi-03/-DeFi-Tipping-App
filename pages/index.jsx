909c995fc8b2ed8009ea23a1f28003200036ee23
Gauranshi Gupta <gauranshigupta2000@gmail.com>
2023-03-23 17:08:55 +0530

    withdrawtips removed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
-import abi from '../utils/BuyMeACoffee.json';
+import abi from "../utils/BuyMeACoffee.json";
 import { ethers } from "ethers";
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   // Contract Address & ABI
@@ -18,11 +18,11 @@ export default function Home() {
 
   const onNameChange = (event) => {
     setName(event.target.value);
-  }
+  };
 
   const onMessageChange = (event) => {
     setMessage(event.target.value);
-  }
+  };
   // eth_accounts is a method of the Ethereum provider API that returns a list of the currently connected Ethereum accounts. This method does not request permission to connect to the user's Ethereum wallet, and it only returns the accounts that have already been granted permission to the website. If no accounts are connected, it returns an empty array.
 
   // eth_requestAccounts, on the other hand, is a method that requests permission from the user to connect to their Ethereum wallet. It prompts the user to choose an account and grants permission to the website to interact with that account. If the user accepts, it returns an array of accounts that can be used to sign transactions and interact with the Ethereum network.
@@ -32,7 +32,7 @@ export default function Home() {
     try {
       const { ethereum } = window;
 
-      const accounts = await ethereum.request({ method: 'eth_accounts' })
+      const accounts = await ethereum.request({ method: "eth_accounts" });
       console.log("accounts: ", accounts);
 
       if (accounts.length > 0) {
@@ -44,7 +44,7 @@ export default function Home() {
     } catch (error) {
       console.log("error: ", error);
     }
-  }
+  };
 
   const connectWallet = async () => {
     try {
@@ -55,14 +55,14 @@ export default function Home() {
       }
 
       const accounts = await ethereum.request({
-        method: 'eth_requestAccounts'
+        method: "eth_requestAccounts",
       });
 
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const buyCoffee = async () => {
     try {
@@ -77,7 +77,7 @@ export default function Home() {
           signer
         );
 
-        console.log("buying coffee..")
+        console.log("buying coffee..");
         const coffeeTxn = await buyMeACoffee.buyCoffee(
           name ? name : "anon",
           message ? message : "Enjoy your coffee!",
@@ -98,26 +98,6 @@ export default function Home() {
       console.log(error);
     }
   };
-    const withdrawTips = async() => {
-    try{
-      const { ethereum } = window;
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum, "any");
-        const signer = provider.getSigner();
-        const buyMeACoffee = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          signer
-        );
-        console.log("Withdrawing Tips");
-        const tx = await buyMeACoffee.withdrawTips();
-        await tx.wait();
-        console.log('Tips withdrawn successfully!');
-      }
-    }catch(error) {
-      console.log(error);
-    }
-  }
 
   // Function to fetch all memos stored on-chain.
   const getMemos = async () => {
@@ -138,7 +118,6 @@ export default function Home() {
       } else {
         console.log("Metamask is not connected");
       }
-
     } catch (error) {
       console.log(error);
     }
@@ -159,8 +138,8 @@ export default function Home() {
           address: from,
           timestamp: new Date(timestamp * 1000),
           message,
-          name
-        }
+          name,
+        },
       ]);
     };
 
@@ -170,11 +149,7 @@ export default function Home() {
     if (ethereum) {
       const provider = new ethers.providers.Web3Provider(ethereum, "any");
       const signer = provider.getSigner();
-      buyMeACoffee = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        signer
-      );
+      buyMeACoffee = new ethers.Contract(contractAddress, contractABI, signer);
 
       // When a new memo event is emitted, the onNewMemo function is called with the event's parameters.
       buyMeACoffee.on("NewMemo", onNewMemo);
@@ -183,11 +158,14 @@ export default function Home() {
       if (buyMeACoffee) {
         buyMeACoffee.off("NewMemo", onNewMemo);
       }
-    }
+    };
   }, []);
 
   return (
-    <div className={styles.container} style={{ background: "linear-gradient(135deg, #000428 , #004e92 )" }}>
+    <div
+      className={styles.container}
+      style={{ background: "linear-gradient(135deg, #000428 , #004e92 )" }}
+    >
       <Head>
         <title>DeFi Tipping Dapp!</title>
         <meta name="description" content="Tipping site" />
@@ -203,9 +181,7 @@ export default function Home() {
           <div>
             <form>
               <div>
-                <label>
-                  Name
-                </label>
+                <label>Name</label>
                 <br />
 
                 <input
@@ -213,7 +189,13 @@ export default function Home() {
                   type="text"
                   placeholder="Enter your name"
                   onChange={onNameChange}
-                  style={{ color: "black", borderRadius: "4px", padding: "12px", boxSizing: "border-box", border: "none" }}
+                  style={{
+                    color: "black",
+                    borderRadius: "4px",
+                    padding: "12px",
+                    boxSizing: "border-box",
+                    border: "none",
+                  }}
                 />
               </div>
               <br />
@@ -230,50 +212,76 @@ export default function Home() {
                   id="message"
                   onChange={onMessageChange}
                   required
-                  style={{ color: "black", borderRadius: "4px", padding: "12px", boxSizing: "border-box", border: "none" }}
-                >
-                </textarea>
+                  style={{
+                    color: "black",
+                    borderRadius: "4px",
+                    padding: "12px",
+                    boxSizing: "border-box",
+                    border: "none",
+                  }}
+                ></textarea>
               </div>
               <div>
                 <button
                   type="button"
                   onClick={buyCoffee}
-                  style={{ color: "black", borderRadius: "4px", padding: "12px", boxSizing: "border-box", border: "none" }}
+                  style={{
+                    color: "black",
+                    borderRadius: "4px",
+                    padding: "12px",
+                    boxSizing: "border-box",
+                    border: "none",
+                  }}
                 >
                   Send 1 Coffee for 0.001ETH
                 </button>
-              </div><br/>
-              <div>
-                <button
-                  type="button"
-                  onClick={withdrawTips}
-                  style={{ color: "black", borderRadius: "4px", padding: "12px", boxSizing: "border-box", border: "none" }}
-                >
-                  Withdraw Tips
-                </button>
               </div>
+              <br />
             </form>
           </div>
         ) : (
-            <button onClick={connectWallet} style={{ backgroundColor: "#fdfd96", color: "blue", borderRadius: '4px', padding: "12px 24px", fontSize: "16px", transition: "background-color 0.3s ease" }} > Connect your wallet </button>
-          )}
+          <button
+            onClick={connectWallet}
+            style={{
+              backgroundColor: "#fdfd96",
+              color: "blue",
+              borderRadius: "4px",
+              padding: "12px 24px",
+              fontSize: "16px",
+              transition: "background-color 0.3s ease",
+            }}
+          >
+            {" "}
+            Connect your wallet{" "}
+          </button>
+        )}
       </main>
 
-      {currentAccount && (<h1 style={{ color: "white" }}>Memos received</h1>)}
+      {currentAccount && <h1 style={{ color: "white" }}>Memos received</h1>}
 
-      {currentAccount && (memos.map((memo, idx) => {
-        return (
-          <div key={idx} style={{ border: "2px solid white", "borderRadius": "5px", padding: "5px", margin: "5px" }}>
-            <p style={{ "fontWeight": "bold", color: "yellow" }}>"{memo.message}"</p>
-            <p style={{ color: "white" }}>From: {memo.name} at {memo.timestamp.toString()}</p>
-          </div>
-        )
-      }))}
+      {currentAccount &&
+        memos.map((memo, idx) => {
+          return (
+            <div
+              key={idx}
+              style={{
+                border: "2px solid white",
+                borderRadius: "5px",
+                padding: "5px",
+                margin: "5px",
+              }}
+            >
+              <p style={{ fontWeight: "bold", color: "yellow" }}>
+                "{memo.message}"
+              </p>
+              <p style={{ color: "white" }}>
+                From: {memo.name} at {memo.timestamp.toString()}
+              </p>
+            </div>
+          );
+        })}
 
-      <footer className={styles.footer}>
-        Created by crypto seekers
-
-      </footer>
+      <footer className={styles.footer}>Created by crypto seekers</footer>
     </div>
-  )
+  );
 }
