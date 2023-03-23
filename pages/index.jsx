0c020276f8ef8bb5d2e757ffd4c2d56a5cd27495
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2023-03-23 15:29:55 +0530

    withdraw tips

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,6 +23,9 @@ export default function Home() {
   const onMessageChange = (event) => {
     setMessage(event.target.value);
   }
+  // eth_accounts is a method of the Ethereum provider API that returns a list of the currently connected Ethereum accounts. This method does not request permission to connect to the user's Ethereum wallet, and it only returns the accounts that have already been granted permission to the website. If no accounts are connected, it returns an empty array.
+
+  // eth_requestAccounts, on the other hand, is a method that requests permission from the user to connect to their Ethereum wallet. It prompts the user to choose an account and grants permission to the website to interact with that account. If the user accepts, it returns an array of accounts that can be used to sign transactions and interact with the Ethereum network.
 
   // Wallet connection logic
   const isWalletConnected = async () => {
@@ -95,6 +98,26 @@ export default function Home() {
       console.log(error);
     }
   };
+    const withdrawTips = async() => {
+    try{
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const signer = provider.getSigner();
+        const buyMeACoffee = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        console.log("Withdrawing Tips");
+        const tx = await buyMeACoffee.withdrawTips();
+        await tx.wait();
+        console.log('Tips withdrawn successfully!');
+      }
+    }catch(error) {
+      console.log(error);
+    }
+  }
 
   // Function to fetch all memos stored on-chain.
   const getMemos = async () => {
@@ -108,7 +131,6 @@ export default function Home() {
           contractABI,
           signer
         );
-
         console.log("fetching memos from the blockchain..");
         const memos = await buyMeACoffee.getMemos();
         console.log("fetched!");
@@ -154,9 +176,9 @@ export default function Home() {
         signer
       );
 
+      // When a new memo event is emitted, the onNewMemo function is called with the event's parameters.
       buyMeACoffee.on("NewMemo", onNewMemo);
     }
-
     return () => {
       if (buyMeACoffee) {
         buyMeACoffee.off("NewMemo", onNewMemo);
@@ -165,15 +187,15 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ background: "linear-gradient(135deg, #000428 , #004e92 )" }}>
       <Head>
-        <title>Buy crypto seekers a Coffee!</title>
+        <title>DeFi Tipping Dapp!</title>
         <meta name="description" content="Tipping site" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className={styles.title} style={{ color: "yellow" }}>
           Lend me some tokens!
         </h1>
 
@@ -189,15 +211,17 @@ export default function Home() {
                 <input
                   id="name"
                   type="text"
-                  placeholder="anon"
+                  placeholder="Enter your name"
                   onChange={onNameChange}
+                  style={{ color: "black", borderRadius: "4px", padding: "12px", boxSizing: "border-box", border: "none" }}
                 />
               </div>
               <br />
               <div>
-                <label>
+                <label style={{ color: "white" }}>
                   Send crypto seekers a message
                 </label>
+                <br />
                 <br />
 
                 <textarea
@@ -206,6 +230,7 @@ export default function Home() {
                   id="message"
                   onChange={onMessageChange}
                   required
+                  style={{ color: "black", borderRadius: "4px", padding: "12px", boxSizing: "border-box", border: "none" }}
                 >
                 </textarea>
               </div>
@@ -213,31 +238,41 @@ export default function Home() {
                 <button
                   type="button"
                   onClick={buyCoffee}
+                  style={{ color: "black", borderRadius: "4px", padding: "12px", boxSizing: "border-box", border: "none" }}
                 >
                   Send 1 Coffee for 0.001ETH
+                </button>
+              </div><br/>
+              <div>
+                <button
+                  type="button"
+                  onClick={withdrawTips}
+                  style={{ color: "black", borderRadius: "4px", padding: "12px", boxSizing: "border-box", border: "none" }}
+                >
+                  Withdraw Tips
                 </button>
               </div>
             </form>
           </div>
         ) : (
-            <button onClick={connectWallet}> Connect your wallet </button>
+            <button onClick={connectWallet} style={{ backgroundColor: "#fdfd96", color: "blue", borderRadius: '4px', padding: "12px 24px", fontSize: "16px", transition: "background-color 0.3s ease" }} > Connect your wallet </button>
           )}
       </main>
 
-      {currentAccount && (<h1>Memos received</h1>)}
+      {currentAccount && (<h1 style={{ color: "white" }}>Memos received</h1>)}
 
       {currentAccount && (memos.map((memo, idx) => {
         return (
-          <div key={idx} style={{ border: "2px solid", "borderRadius": "5px", padding: "5px", margin: "5px" }}>
-            <p style={{ "fontWeight": "bold" }}>"{memo.message}"</p>
-            <p>From: {memo.name} at {memo.timestamp.toString()}</p>
+          <div key={idx} style={{ border: "2px solid white", "borderRadius": "5px", padding: "5px", margin: "5px" }}>
+            <p style={{ "fontWeight": "bold", color: "yellow" }}>"{memo.message}"</p>
+            <p style={{ color: "white" }}>From: {memo.name} at {memo.timestamp.toString()}</p>
           </div>
         )
       }))}
 
       <footer className={styles.footer}>
         Created by crypto seekers
-     
+
       </footer>
     </div>
   )
